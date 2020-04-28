88043408d7c76d73789abb2aefa837570b94b9d1
cliod <qi2415@qq.com>
2020-04-28 12:06:26 +0800

    add assert

---
+++ development-exception/src/main/java/com/wobangkj/api/Assert.java
@@ -0,0 +1,11 @@
+package com.wobangkj.api;
+
+/**
+ * assert
+ *
+ * @author @cliod
+ * @since 4/28/20 11:53 AM
+ * package: com.wobangkj.api
+ */
+public interface Assert {
+}
---
+++ development-exception/src/main/java/com/wobangkj/api/AuthExceptionAssert.java
@@ -0,0 +1,11 @@
+package com.wobangkj.api;
+
+/**
+ * 业务异常处理接口
+ *
+ * @author @cliod
+ * @since 4/28/20 11:57 AM
+ * package: com.wobangkj.api
+ */
+public interface BusinessExceptionAssert {
+}
