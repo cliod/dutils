916fe5bb9c269162b691dfde0d7f41291bccb00b
cliod <qi2415@qq.com>
2020-07-08 16:04:22 +0800

    更新代码, 添加雪花算法

---
+++ development-jwt/src/main/java/com/wobangkj/api/UidGenerateException.java
@@ -0,0 +1,39 @@
+package com.wobangkj.api;
+
+/**
+ * id生成器异常
+ *
+ * @author cliod
+ * @since 7/8/20 3:37 PM
+ */
+public class UidGenerateException extends RuntimeException {
+
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = -27048199131316992L;
+
+    /**
+     * Default constructor
+     */
+    public UidGenerateException() {
+        super();
+    }
+
+    public UidGenerateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UidGenerateException(String message) {
+        super(message);
+    }
+
+    public UidGenerateException(String msgFormat, Object... args) {
+        super(String.format(msgFormat, args));
+    }
+
+    public UidGenerateException(Throwable cause) {
+        super(cause);
+    }
+
+}
