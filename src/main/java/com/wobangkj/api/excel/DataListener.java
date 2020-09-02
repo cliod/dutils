ac2db315cd07762e24b5c09e634c01b7e2910cb0
cliod <qi2415@qq.com>
2020-09-02 10:26:05 +0800

    fix(daily-report) bug修复
    bug修复

--- src/main/java/com/wobangkj/api/excel/DataListener.java
+++ src/main/java/com/wobangkj/api/excel/DataListener.java
@@ -2,6 +2,8 @@ package com.wobangkj.api.excel;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.wobangkj.ThreadExecutor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
@@ -16,79 +18,87 @@ import java.util.List;
 @Slf4j
 public abstract class DataListener<T extends Model> extends AnalysisEventListener<T> {
 
-    /**
-     * 每隔3000条存储数据库，然后清理list ，方便内存回收
-     */
-    private static final int BATCH_COUNT = 3000;
-    /**
-     * 暂时存储列表
-     */
-    protected final List<Model> dataCache = new ArrayList<>();
-    /**
-     * 每次数据唯一id
-     */
-    protected String uuid;
+	/**
+	 * 每隔3000条存储数据库，然后清理list ，方便内存回收
+	 */
+	private static final int BATCH_COUNT = 3000;
+	/**
+	 * 暂时存储列表
+	 */
+	protected final List<Model> dataCache = new ArrayList<>();
+	/**
+	 * 每次数据唯一id
+	 */
+	@Setter
+	protected String id;
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+	@Deprecated
+	public void setUuid(String uuid) {
+		this.id = uuid;
+	}
 
-    @Override
-    public void invoke(T data, AnalysisContext context) {
-        process(data, context);
-    }
+	@Override
+	public void invoke(T data, AnalysisContext context) {
+		this.process(data, context);
+	}
 
-    protected void process(Model data, AnalysisContext context) {
-        if (this.filter(data, context)) {
-            return;
-        }
-        // 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
-        if (dataCache.size() >= BATCH_COUNT) {
-            process(context);
-            // 处理完成清理缓存
-            dataCache.clear();
-        }
-    }
+	@Override
+	public void doAfterAllAnalysed(AnalysisContext context) {
+		//将剩余的数据进行处理
+		ThreadExecutor.fork.execute(() -> {
+			process(context);
+			dataCache.clear();
+		});
+	}
 
-    @Override
-    public void doAfterAllAnalysed(AnalysisContext context) {
-        //将剩余的数据进行处理
-        process(context);
-    }
+	protected void process(Model data, AnalysisContext context) {
+		if (this.filter(data, context)) {
+			return;
+		}
+		dataCache.add(data);
+		// 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
+		if (dataCache.size() >= BATCH_COUNT) {
+			ThreadExecutor.fork.execute(() -> {
+				process(context);
+				// 处理完成清理缓存
+				dataCache.clear();
+			});
+		}
+	}
 
-    /**
-     * 加上存储数据库
-     *
-     * @param context 上下文内容
-     */
-    protected abstract void process(AnalysisContext context);
+	/**
+	 * 加上存储数据库
+	 *
+	 * @param context 上下文内容
+	 */
+	protected abstract void process(AnalysisContext context);
 
-    /**
-     * 在添加之前进行筛选
-     *
-     * @param data    数据
-     * @param context 上下文内容
-     */
-    protected boolean filter(Model data, AnalysisContext context) {
-        return false;
-    }
+	/**
+	 * 在添加之前进行筛选
+	 *
+	 * @param data    数据
+	 * @param context 上下文内容
+	 */
+	protected boolean filter(Model data, AnalysisContext context) {
+		return false;
+	}
 
-    /**
-     * 获取成功标识
-     *
-     * @return 字符串
-     */
-    protected String getFailedTag() {
-        return "";
-    }
+	/**
+	 * 获取成功标识
+	 *
+	 * @return 字符串
+	 */
+	protected String getFailedTag() {
+		return "";
+	}
 
-    /**
-     * 获取excel头标识
-     *
-     * @return 字符串
-     */
-    protected String getTitleTag() {
-        return "";
-    }
+	/**
+	 * 获取excel头标识
+	 *
+	 * @return 字符串
+	 */
+	protected String getTitleTag() {
+		return "";
+	}
 
 }
