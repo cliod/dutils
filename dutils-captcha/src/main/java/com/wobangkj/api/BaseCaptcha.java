4f667570d5a5376b54299232229984e01cc5e7f3
cliod <qi2415@qq.com>
2020-09-14 13:08:57 +0800

    feat(daily-report) 优化
    图片验证码使用糊涂

---
+++ dutils-captcha/src/main/java/com/wobangkj/api/BaseCaptcha.java
@@ -0,0 +1,42 @@
+package com.wobangkj.api;
+
+import cn.hutool.captcha.AbstractCaptcha;
+import lombok.SneakyThrows;
+
+import java.awt.*;
+
+/**
+ * 验证码默认实现
+ *
+ * @author cliod
+ * @since 9/14/20 11:20 AM
+ */
+public abstract class BaseCaptcha extends AbstractCaptcha implements Captcha {
+
+	protected AbstractCaptcha delegate; //委托
+
+	/**
+	 * 构造，使用随机验证码生成器生成验证码
+	 *
+	 * @param width          图片宽
+	 * @param height         图片高
+	 * @param codeCount      字符个数
+	 * @param interfereCount 验证码干扰元素个数
+	 */
+	@SneakyThrows
+	public BaseCaptcha(int width, int height, int codeCount, int interfereCount, Class<? extends AbstractCaptcha> delegate) {
+		super(width, height, codeCount, interfereCount);
+		this.delegate = delegate.getConstructor(int.class, int.class, int.class, int.class).newInstance(width, height, codeCount, interfereCount);
+	}
+
+	/**
+	 * 根据生成的code创建验证码图片
+	 *
+	 * @param code 验证码
+	 * @return Image
+	 */
+	@Override
+	protected Image createImage(String code) {
+		return delegate.getImage();
+	}
+}
---
+++ dutils-captcha/src/main/java/com/wobangkj/api/Captcha.java
@@ -0,0 +1,12 @@
+package com.wobangkj.api;
+
+import cn.hutool.captcha.ICaptcha;
+
+/**
+ * 验证码接口
+ *
+ * @author cliod
+ * @since 9/14/20 11:20 AM
+ */
+public interface Captcha extends ICaptcha {
+}
