cef043d9fd2a13297c74c07f7a1bd61dd30a9625
cliod <qi2415@qq.com>
2020-10-10 14:18:03 +0800

    feat(4.1.1-SNAPSHOT): 添加抽象
    添加一个excel读取处理卡抽象, 简单同步处理读取结果

--- dutils-excel/src/main/java/com/wobangkj/api/SyncReadSimpleListener.java
+++ dutils-excel/src/main/java/com/wobangkj/api/SyncReadSimpleListener.java
@@ -10,46 +10,44 @@ import lombok.extern.slf4j.Slf4j;
  * @since 7/9/20 5:03 PM
  */
 @Slf4j
-public abstract class SyncReadProcessListener<T> extends SyncReadListener<T> {
+public abstract class SyncReadSimpleListener<T> extends SyncReadListener<T> {
 
 	@Override
 	public final void invoke(T data, AnalysisContext context) {
-		if (!this.filter(data, context)) {
+		if (!this.filter(data)) {
 			return;
 		}
 		this.cache.add(data);
-		if (this.condition(context)) {
+		if (this.condition()) {
 			// 满足条件, 提前处理
-			this.process(context);
+			this.process();
 		}
 		// 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
 		if (this.cache.size() >= getMax()) {
-			this.process(context);
+			this.process();
 		}
 	}
 
 	/**
 	 * 满足的条件
 	 *
-	 * @param context 上下文内容
 	 * @return 是否满足
 	 */
-	protected boolean condition(AnalysisContext context) {
+	private boolean condition() {
 		return false;
 	}
 
 	@Override
 	public final void doAfterAllAnalysed(AnalysisContext context) {
-		this.process(context);
+		this.process();
 	}
 
 	/**
 	 * 在添加之前进行筛选
 	 *
-	 * @param data    数据
-	 * @param context 上下文内容
+	 * @param data 数据
 	 */
-	protected boolean filter(T data, AnalysisContext context) {
+	protected boolean filter(T data) {
 		return true;
 	}
 
@@ -65,36 +63,30 @@ public abstract class SyncReadProcessListener<T> extends SyncReadListener<T> {
 	/**
 	 * 处理逻辑
 	 */
-	public final void process(AnalysisContext context) {
+	public final void process() {
 		// 之前
-		this.before(context);
+		this.before();
 		// 处理
-		this.doProcess(context);
+		this.doProcess();
 		// 之后
-		this.after(context);
+		this.after();
 	}
 
 	/**
 	 * 之前
-	 *
-	 * @param context 参数
 	 */
-	protected void before(AnalysisContext context) {
+	protected void before() {
 	}
 
 	/**
 	 * 真正处理
-	 *
-	 * @param context 参数
 	 */
-	protected abstract void doProcess(AnalysisContext context);
+	protected abstract void doProcess();
 
 	/**
 	 * 之后
-	 *
-	 * @param context 参数
 	 */
-	protected void after(AnalysisContext context) {
+	protected void after() {
 		this.cache.clear();
 	}
 }
